a38894ffcf79ed4da57f2c7b24339b31c9d5b2a8
RMx-03 <140522220+RMx-03@users.noreply.github.com>
2025-04-04 00:28:59 +0530

    Minor changes in backend, adding the frontend

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -7,7 +7,7 @@ export const getUsersForSidebar = async (req,res) => {
         const loggedInUserId = req.user._id;
         const filteredUsers = await User.find({_id: {$ne:loggedInUserId}}).select("-password");
         
-        res.status(200).json(filteredUsers)
+        res.status(200).json(filteredUsers);
     } catch (error) {
         console.error("Error in getUsersForSidebar: ", error.message);
         res.status(500).json({ error: "Internal server error" });
@@ -16,7 +16,7 @@ export const getUsersForSidebar = async (req,res) => {
 
 export const getMessages = async (req,res) => {
     try {
-        const { id:userToChatId } = req.params
+        const { id:userToChatId } = req.params;
         const myId = req.user._id;
 
         const messages = await Message.find({
@@ -24,9 +24,9 @@ export const getMessages = async (req,res) => {
                 {senderId:myId, receiverId:userToChatId}, 
                 {senderId:userToChatId, receiverId:myId}
             ]
-        })
+        });
 
-        res.status(200).json(messages)
+        res.status(200).json(messages);
     } catch (error) {
         console.log("Error in getMessages controller: ", error.message);
         res.status(500).json({ error: "Internal server error" });
